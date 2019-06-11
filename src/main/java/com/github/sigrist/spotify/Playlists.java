a52b09e7c1add4aad812b134b5a9b06db82035d1
Paulo Sigrist <paulo.sigrist@gmail.com>
2019-06-11 14:43:42 -0300

    For #26

--- src/main/java/com/github/sigrist/spotify/Playlists.java
+++ src/main/java/com/github/sigrist/spotify/Playlists.java
@@ -1,9 +1,48 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2019 Paulo Sigrist
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 package com.github.sigrist.spotify;
 
+/**
+ * A collection of {@link Playlist}.
+ * @since 1.0.0
+ */
 public interface Playlists extends Iterable<Playlist> {
+    /**
+     * The page size.
+     * @return The page size.
+     */
     Integer limit();
 
+    /**
+     * The offset.
+     * @return The offset.
+     */
     Integer offset();
 
+    /**
+     * The total of elements.
+     * @return The total of elements.
+     */
     Integer total();
 }
