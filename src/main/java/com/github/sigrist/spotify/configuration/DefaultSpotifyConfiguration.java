77b532664bcde516ac5f3f993bdb084227436242
Paulo Sigrist <paulo.sigrist@gmail.com>
2019-06-11 11:15:53 -0300

    Default SpotifyConfiguration impl
    For #12

---
+++ src/main/java/com/github/sigrist/spotify/configuration/DefaultSpotifyConfiguration.java
@@ -0,0 +1,46 @@
+package com.github.sigrist.spotify.configuration;
+
+import com.github.sigrist.spotify.SpotifyConfiguration;
+
+/**
+ * Default configuration.
+ * 
+ * @author sigrist
+ *
+ */
+public class DefaultSpotifyConfiguration implements SpotifyConfiguration {
+
+	/** The user token to access the API. */
+	private final String token;
+
+	/**
+	 * Default constructor.
+	 * 
+	 * @param token The user token to access the API.
+	 */
+	public DefaultSpotifyConfiguration(final String token) {
+		this.token = token;
+	}
+
+	/**
+	 * Returns the default value.
+	 * @param key The configuration key.
+	 * @param defaultValue The default value if not found.
+	 * @return Always the defaultValue
+	 */
+	@Override
+	public String get(final String key, final String defaultValue) {
+		return defaultValue;
+	}
+
+	@Override
+	public String token() {
+		return token;
+	}
+
+	@Override
+	public String server() {
+		return "https://api.spotify.com";
+	}
+
+}
