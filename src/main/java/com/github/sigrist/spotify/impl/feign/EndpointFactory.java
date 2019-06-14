72c7c75357dcf2535fa8a91d4293611402d8e99b
Paulo Sigrist <paulo.sigrist@gmail.com>
2019-06-14 10:51:54 -0300

    Javadoc
    for #30

--- src/main/java/com/github/sigrist/spotify/impl/feign/EndpointFactory.java
+++ src/main/java/com/github/sigrist/spotify/impl/feign/EndpointFactory.java
@@ -5,6 +5,9 @@ import feign.Feign;
 import feign.jackson.JacksonDecoder;
 import feign.jackson.JacksonEncoder;
 
+/**
+ * Factory class to create the {@link Feign} instances.
+ */
 public class EndpointFactory {
 
     /**
@@ -17,13 +20,21 @@ public class EndpointFactory {
      */
     private final Spotify spotify;
 
+    /**
+     * Default constructor.
+     * @param spotify The spotify instance.
+     */
     public EndpointFactory(final Spotify spotify) {
         this.spotify = spotify;
         this.builder = Feign.builder().decoder(new JacksonDecoder()).encoder(new JacksonEncoder());
 
     }
-    
-    
+
+    /**
+     * Builds the Feign instance.
+     * @param clazz The class that should be build.
+     * @return An instance of the class.
+     */
     public <T> T build(final Class<T> clazz) {
         final String server = this.spotify.conf().getServer();
         return this.builder.target(clazz, server);
